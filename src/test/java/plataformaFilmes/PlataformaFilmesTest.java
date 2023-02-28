ccc3142df4f3e4808f2ee9321def5ecfb218298c
jlamancio <amancio.jluis@outlook.com>
2023-02-27 21:01:50 -0300

    aula13 Cucumber Runner Pacotes

--- src/test/java/plataformaFilmes/PlataformaFilmesTest.java
+++ src/test/java/plataformaFilmes/PlataformaFilmesTest.java
@@ -1,5 +1,6 @@
 package plataformaFilmes;
 
+import Maps.LoginMap;
 import io.restassured.http.ContentType;
 import io.restassured.response.Response;
 import org.junit.jupiter.api.Assertions;
@@ -19,17 +20,12 @@ public class PlataformaFilmesTest {
     public void validarLogin(){
 
         RestUtils.setBaseUri("http://amancio:8080/");          /* URL e Endpoint   */
+        LoginMap.initLogin();
 
-        String json = "{" +
-                "    \"email\": \"aluno@email.com\"," +
-                "    \"senha\": \"123456\"" +
-                "}";
+          Response response = RestUtils.post(LoginMap.getLogin(), ContentType.JSON,  "auth");
 
-          Response response = RestUtils.post(json, ContentType.JSON,  "auth");
-
-            Assertions.assertEquals(200, response.statusCode());
-            String token = response.jsonPath().get("token");
-            System.out.println(token);
+          Assertions.assertEquals(200, response.statusCode());
+          LoginMap.token = response.jsonPath().get("token");
 
     }
 
@@ -37,31 +33,28 @@ public class PlataformaFilmesTest {
     public static void validarLoginMap(){
 
         RestUtils.setBaseUri("http://amancio:8080/");          /* URL e Endpoint   */
-        Map<String, String> map = new HashMap<>();
-        map.put("email", "aluno@email.com");
-        map.put("senha", "123456");
+        LoginMap.initLogin();
 
-
-        Response response = RestUtils.post(map, ContentType.JSON,  "auth");
+        Response response = RestUtils.post(LoginMap.getLogin(), ContentType.JSON,  "auth");
 
         Assertions.assertEquals(200, response.statusCode());
-        token = response.jsonPath().get("token");
-        System.out.println(token);
+        LoginMap.token = response.jsonPath().get("token");
+
     }
     
     @Test
     public void validarConsultaCategorias(){
         
         Map<String, String> header = new HashMap<>();
-        header.put("Authorization", "Bearer "+token);
+        header.put("Authorization", "Bearer "+LoginMap.token);
 
         Response response = RestUtils.get(header, "categorias");
         Assertions.assertEquals(200, response.statusCode());
 
         Assertions.assertEquals("Terror", response.jsonPath().get("tipo[2]"));
 
-        List<String> tiposDeFilmes = response.jsonPath().get("tipo");
-        Assertions.assertTrue(tiposDeFilmes.contains("Terror"), "Não foi encontrada a categoria Romance");
+        List<String> listTipo = response.jsonPath().get("tipo");
+        Assertions.assertTrue(listTipo.contains("Terror"),"Não foi encontrada a categoria Romance");
 
     }
 
---
+++ src/test/java/steps/LoginSteps.java
@@ -0,0 +1,23 @@
+package steps;
+
+import io.cucumber.java.pt.Dado;
+import io.cucumber.java.pt.Quando;
+
+public class LoginSteps {
+
+    @Dado("que tenha payload valido da API de Login")
+    public void queTenhaPayloadValidoDaAPIDeLogin() {
+
+        // Write code here that turns the phrase above into concrete actions
+        throw new io.cucumber.java.PendingException();
+
+    }
+    @Quando("envio uma requisição do tipo POST de Login")
+    public void envioUmaRequisiçãoDoTipoPOSTDeLogin() {
+
+        // Write code here that turns the phrase above into concrete actions
+        throw new io.cucumber.java.PendingException();
+
+    }
+
+}
